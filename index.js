bcbc035293fcb8f20070918483b2be213d962394
godsonizhere <154236474+godsonizhere@users.noreply.github.com>
2024-11-12 17:35:48 +0000

    color change

--- index.js
+++ index.js
@@ -4,40 +4,40 @@ let converter = {
     kilogram: 2.204
 };
 
-const inputEl = document.getElementById("input-el");
-const buttonEl = document.getElementById("button-el");
-const alertEl = document.getElementById("alert-el");
-const meterEl = document.getElementById("meter-el");
-const volumeEl = document.getElementById("volume-el");
-const massEl = document.getElementById("mass-el");
+const inputEl = document.getElementById("input-el")
+const buttonEl = document.getElementById("button-el")
+const alertEl = document.getElementById("alert-el")
+const meterEl = document.getElementById("meter-el")
+const volumeEl = document.getElementById("volume-el")
+const massEl = document.getElementById("mass-el")
 
 function render() {
     if (inputEl.value.trim() === "") {
-        alertEl.textContent = "Please enter a value.";
+        alertEl.innerHTML = `<p>Please enter a valid number.</p>`
         return;
     }
 
-    const inputValue = parseFloat(inputEl.value);
+    const inputValue = parseFloat(inputEl.value)
     if (isNaN(inputValue)) {
-        alertEl.textContent = "Please enter a valid number.";
+        alertEl.innerHTML = `<p>Please enter a valid number.</p>`
         return;
     }
 
-    alertEl.textContent = "";
+    alertEl.textContent = ""
     
-    let meter = inputValue * converter.meter;
-    let feet = inputValue / converter.meter;
-    meterEl.innerHTML = `<p>${inputValue} meters = ${meter.toFixed(2)} feet | ${inputValue} feet = ${feet.toFixed(2)} meters</p>`;
+    let meter = inputValue * converter.meter
+    let feet = inputValue / converter.meter
+    meterEl.innerHTML = `<p>${inputValue} meters = ${meter.toFixed(2)} feet | ${inputValue} feet = ${feet.toFixed(2)} meters</p>`
 
-    let liter = inputValue * converter.liter;
-    let gallon = inputValue / 3.785;
-    volumeEl.innerHTML = `<p>${inputValue} liters = ${liter.toFixed(2)} gallons | ${inputValue} gallons = ${gallon.toFixed(2)} liters</p>`;
+    let liter = inputValue * converter.liter
+    let gallon = inputValue / 3.785
+    volumeEl.innerHTML = `<p>${inputValue} liters = ${liter.toFixed(2)} gallons | ${inputValue} gallons = ${gallon.toFixed(2)} liters</p>`
 
-    let kilogram = inputValue * converter.kilogram;
-    let pound = inputValue * 2.204;
-    massEl.innerHTML = `<p>${inputValue} kilos = ${kilogram.toFixed(2)} pounds | ${inputValue} pounds = ${pound.toFixed(2)} kilos</p>`;
+    let kilogram = inputValue * converter.kilogram
+    let pound = inputValue * 2.204
+    massEl.innerHTML = `<p>${inputValue} kilos = ${kilogram.toFixed(2)} pounds | ${inputValue} pounds = ${pound.toFixed(2)} kilos</p>`
 }
 
 buttonEl.addEventListener("click", function() {
-    render();
-});
+    render()
+})
